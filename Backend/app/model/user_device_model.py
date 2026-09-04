11aefcdbff18840d47f8da24950254a295057064
Ashu8083n <asutosh.gouda22@gmail.com>
2026-09-04 22:44:18 +0530

    device model added

---
+++ Backend/app/model/user_device_model.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, DateTime, Integer, ForeignKey,UUID
+import uuid
+from sqlalchemy.orm import Mapped,mapped_column,relationship
+
+from app.core.database import Base
+
+class UserDeviceModel(Base):
+    __tablename__ = "user_device"
+    id : Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+    )
+    user_id : Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("user.id", ondelete="CASCADE"),
+    )
+    device_id : Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+    )
+
+
--- Backend/app/model/user_model.py
+++ Backend/app/model/user_model.py
@@ -1,43 +1,55 @@
 import uuid
+from datetime import datetime
 
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base
 
 
-
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
         primary_key=True,
-        default=uuid.uuid4
+        default=uuid.uuid4,
     )
-
     username: Mapped[str] = mapped_column(
         String(100),
         unique=True,
         nullable=False,
-        index=True
+        index=True,
     )
-
     email: Mapped[str] = mapped_column(
         String(225),
         unique=True,
         nullable=False,
-        index=True
+        index=True,
     )
-
     password_hash: Mapped[str] = mapped_column(
         String(225),
-        nullable=False
+        nullable=False,
     )
-
     is_active: Mapped[bool] = mapped_column(
         Boolean,
         default=True,
-        nullable=False
+        nullable=False,
+    )
+    is_deleted: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
     )
