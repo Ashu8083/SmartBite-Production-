90f5641c8557888725432724b13dcf3d81480679
PRITI1412 <pritymayeerout@gmail.com>
2026-09-05 18:22:08 +0530

    gender enums created

--- Backend/app/enums/user_enums.py
+++ Backend/app/enums/user_enums.py
@@ -3,4 +3,8 @@ from enum import Enum
 class UserStatus(Enum,str):
     ACTIVE = "ACTIVE"
     INACTIVE = "INACTIVE"
-    
+
+class Gender(Enum,str):
+    MALE="MALE"
+    FEMALE="FEMALE"
+    OTHER="OTHER"
