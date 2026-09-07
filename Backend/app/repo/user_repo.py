273ce14c702c16b8853b8b2f31ba610d734c77cf
sonali43 <sonalisethi439@gmail.com>
2026-09-07 17:12:49 +0530

    dev-sona:create packaged_food_schema and repo file

--- Backend/app/repo/user_repo.py
+++ Backend/app/repo/user_repo.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from uuid import UUID
-from Backend.app.repo.user_repo import Users
+from Backend.app.model.user_model import Users
 class UserRepository:
     def _init_(self,db:Session):
         self.db=db
