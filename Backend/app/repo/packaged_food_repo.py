8e4e32160a5ba156fde46899d2dbeb48a0221926
sonali43 <sonalisethi439@gmail.com>
2026-09-11 18:57:03 +0530

    dev-sona : packaged_food_repo created

--- Backend/app/repo/packaged_food_repo.py
+++ Backend/app/repo/packaged_food_repo.py
@@ -1,6 +1,21 @@
 from sqlalchemy.orm import Session
-from Backend.app.model.user_model import Users
+from Backend.app.model.package_food_model import PackageFood
+from Backend.app.schema.packaged_food_schema import PackagedFoodSchema 
 
 class PackagedFoodRepository:
     def __init__(self,db:Session):
         self.db=db
+        
+    def create_packaged_food_repo(self,create_packagedFood:PackagedFoodSchema,id:int):
+        packagedfood=PackageFood(
+            id=create_packagedFood.id,
+            name=create_packagedFood.name,
+            description=create_packagedFood.description,
+            price=create_packagedFood.price
+        )
+        
+        self.db.add(packagedfood)
+        self.db.flush()
+        return packagedfood
+    
+    
