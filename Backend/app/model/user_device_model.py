1dd7d7311497f0996fd9d5a029efa30cd0466a58
Ashu8083n <asutosh.gouda22@gmail.com>
2026-09-11 17:36:37 +0530

    dev-ashu : user device model update and user status and gender add

--- Backend/app/model/user_device_model.py
+++ Backend/app/model/user_device_model.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, DateTime, Integer, ForeignKey,UUID
 import uuid
+from datetime import datetime
 from sqlalchemy.orm import Mapped,mapped_column,relationship
+from sqlalchemy import Column, DateTime, Integer, ForeignKey,UUID ,String ,Boolean
 
 from app.core.database import Base
+from app.model.timestamp import TimestampMixin
 
-class UserDevice(Base):
+
+class UserDevice(Base,TimestampMixin):
     __tablename__ = "user_device"
     id : Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
@@ -18,7 +21,16 @@ class UserDevice(Base):
     device_id : Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
     )
+    device_type: Mapped[str] = mapped_column(String)
+    firebase_fcm_token: Mapped[str] = mapped_column(String)
 
+    last_login: Mapped[datetime] = mapped_column(DateTime)
+    logout_time: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+    is_login: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True
+    )
     users = relationship("Users",
                          back_populates="user_device",)
 
