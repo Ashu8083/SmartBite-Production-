f50aa5e95c60ad5911698071625b7beee9fea483
sonali43 <sonalisethi439@gmail.com>
2026-09-12 19:03:27 +0530

    dev-sona:packaged_food_repo completed and packaged_food_schema completed

--- Backend/app/schema/packaged_food_schema.py
+++ Backend/app/schema/packaged_food_schema.py
@@ -1,8 +1,42 @@
 from pydantic import BaseModel
 
 class PackagedFoodSchema(BaseModel):
-    
-    id : int
-    name : str
-    description : str
+    brand_id : str
+    barcode : str
+    image_url : str |None=None
     price : float
+    category_id:int
+    allergens_id : int
+    serving_size : float
+    serving_unit : str | None=None
+    quantity : float
+    quantity_unit : str | None=None
+    food_claims : str | None = None
+    
+class UpdatePackageFood(BaseModel):
+    brand_id : str
+    barcode : str
+    image_url : str |None=None
+    price : float
+    category_id:int
+    allergens_id : int
+    serving_size : float
+    serving_unit : str | None=None
+    quantity : float
+    quantity_unit : str | None=None
+    food_claims : str | None = None
+    
+class PackageFoodResponse(BaseModel):
+    id : int
+    brand_id : str
+    barcode : str
+    image_url : str |None=None
+    price : float
+    category_id:int
+    allergens_id : int
+    serving_size : float
+    serving_unit : str | None=None
+    quantity : float
+    quantity_unit : str | None=None
+    food_claims : str | None = None
+    
