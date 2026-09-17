3d1bf97d07610da364345d6197866e122ef48cd0
sonali43 <sonalisethi439@gmail.com>
2026-09-17 19:16:38 +0530

    dev-sona:add in service_dependency and create api for food_allergen

--- Backend/app/dependency/service_dependency.py
+++ Backend/app/dependency/service_dependency.py
@@ -3,6 +3,8 @@ from app.repo.user_repo import UserRepository
 from app.service.user_service import UserService 
 from app.repo.packaged_food_repo import PackagedFoodRepository
 from app.service.package_food_service import PackagedFoodService
+from app.repo.packaged_food_allergen_repo import PackageFoodAllergenRepository
+from app.service.package_food_allergen_service import PackageFoodAllergenService
 from app.core.database import get_db
 
 
@@ -15,3 +17,8 @@ def get_package_food_service(db=Depends(get_db)):
     package_food_repository=PackagedFoodRepository(db)
     package_food_service=PackagedFoodService(package_food_repository)
     return package_food_service
+
+def get_package_food_allergen_service(db=Depends(get_db)):
+    package_food_allergen_repo=PackageFoodAllergenRepository(db)
+    package_food_allergen_service=PackageFoodAllergenService(package_food_allergen_repo)
+    return package_food_allergen_service
