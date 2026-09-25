dc84fc45689f833c819c2d6ebc3a2b8a5f6a6189
sonali43 <sonalisethi439@gmail.com>
2026-09-25 17:48:53 +0530

    dev-sona: added nutrient api file in main file

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -5,6 +5,7 @@ from app.api.user_device_api import user_device_router
 from app.api.package_food_api import packaged_food_router
 from app.api.package_food_allergen_api import package_food_allergen_router
 from app.api.package_food_nutrition_api import package_food_nutrition_router
+from app.api.nutrient_api import nutrient_router
 from app.api.allergen_api import allergen_router
 from app.api.brand_api import brand_router
 
@@ -20,6 +21,7 @@ app.include_router(user_device_router)
 app.include_router(packaged_food_router)
 app.include_router(package_food_allergen_router)
 app.include_router(package_food_nutrition_router)
+app.include_router(nutrient_router)
 app.include_router(allergen_router)
 app.include_router(brand_router)
 
